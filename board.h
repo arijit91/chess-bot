a712a8b8d1afab30c3bfe5466c06e57d542e5758
Arijit Banerjee <arijit91@gmail.com>
2013-10-20 14:14:23 -0700

    Fixed whitespaces.

--- board.h
+++ board.h
@@ -7,19 +7,19 @@
 using namespace std;
 
 class Board {
-    piece_type board[64];
-    //square_type pieces[13];
-    square_type enp_square;
-    bitset<64> pawns[2];
-    bitset<4> castle_rights;
-    turn_type turn;
-    int halfmove;
-    int fullmove;
+  piece_type board[64];
+  //square_type pieces[13];
+  square_type enp_square;
+  bitset<64> pawns[2];
+  bitset<4> castle_rights;
+  turn_type turn;
+  int halfmove;
+  int fullmove;
 
-    public:
-    string sqtostr(int);
+  public:
+  string sqtostr(int);
 
-    void setPositionFromFEN(string);
-    void printBoard(bool castle = true, bool enp = true, bool moves = true);
+  void setPositionFromFEN(string);
+  void printBoard(bool castle = true, bool enp = true, bool moves = true);
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ string startFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq e6 0 1";
 string fen2 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2";
 
 int main() {
-    Board b;
-    b.setPositionFromFEN(startFEN);
-    b.setPositionFromFEN(fen2);
-    b.printBoard();
-    return 0;
+  Board b;
+  b.setPositionFromFEN(startFEN);
+  b.setPositionFromFEN(fen2);
+  b.printBoard();
+  return 0;
 }
